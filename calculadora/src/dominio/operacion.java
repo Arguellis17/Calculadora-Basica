7a56838c390031ad0cc6fa26ed5491918095631e
JuanArguello <arguelloj866@gmail.com>
2023-04-02 19:43:24 -0500

    Se han agregado los atributos y metodos de la clase operacion

--- calculadora/src/dominio/operacion.java
+++ calculadora/src/dominio/operacion.java
@@ -3,5 +3,51 @@ package dominio;
 
 
 public class operacion {
+    // Atributos
+    private int a;
+    private int b;
+    
+    // Constructor
+    public operacion(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    
+    // Metodos consultores
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    
+    // Metodos operacionales
+    public int sumar(){
+        
+        return (a + b);
+    }
+    public int restar(){
+        
+        return (a - b);
+    }
+    public int multiplicar(){
+        
+        return (a * b);
+   }
+    public double dividir(){
+        
+        return ((double) a / b);
+    }
+    
+           
     
 }
